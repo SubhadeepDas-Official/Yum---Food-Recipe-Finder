f7ccd567af2c0833d276b5eecc4f431fcae84cc2
Subhadeep Das <68593264+SubhadeepDas-Official@users.noreply.github.com>
2025-07-04 05:02:32 +0530

    test

--- Mobile/app/_layout.jsx
+++ Mobile/app/_layout.jsx
@@ -1,11 +1,29 @@
+// import { Slot } from "expo-router";
+// import { ClerkProvider } from '@clerk/clerk-expo'
+// import { tokenCache } from '@clerk/clerk-expo/token-cache'
+// import SafeScreen from "@/components/SafeScreen"
+
+// export default function RootLayout() {
+//   return (
+//     <ClerkProvider tokenCache={tokenCache}>
+//       <SafeScreen>
+//         <Slot />
+//       </SafeScreen>
+//     </ClerkProvider>
+//   );
+// }
+
 import { Slot } from "expo-router";
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import SafeScreen from "@/components/SafeScreen"
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import SafeScreen from "@/components/SafeScreen";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
       <SafeScreen>
         <Slot />
       </SafeScreen>

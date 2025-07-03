749f9b15166a95f4e3e155378904216ba17a9f31
Subhadeep Das <68593264+SubhadeepDas-Official@users.noreply.github.com>
2025-07-04 02:13:07 +0530

    final commit

--- Mobile/constants/api.js
+++ Mobile/constants/api.js
@@ -1,3 +1,4 @@
 // when using physical phone, change to the deployed url
 // export const API_URL = "http://localhost:3000/api";
-export const API_URL = "https://yum-food-recipe-finder-hnjq.onrender.com";
+// export const API_URL = "https://yum-food-recipe-finder-hnjq.onrender.com";
+export const API_URL = "https://yum-food-recipe-finder-hnjq.onrender.com/api";
